7420592672e084be8433001b63e2462c6dc74fd0
bpetterborg <ben@ben-p.dev>
2021-04-11 13:55:39 -0400

    [INCOMPLETE] Add: open token file and start bot

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python3
+#
+# REMINDER: DO NOT PUT THE SECRETS ON THE PUBLIC REPO
+# 
+# This is a discord bot to listen to radio traffic from an RTL-SDR
+# and rebroadcast it to discord. 
 
+
+# imports
+import os           # not sure what this is
+import discord      # for the bot
+from discord.ext import commands	# to use commands to control bot
+
+# variables
+client = commands.Bot(command_prefix = '.radio') # the prefix to the command
+
+# get token (make sure token file is added to git ignore)
+token = open('token', 'r')
+
+
+@client.event
+async def on_ready(): # start when bot is in ready state
+	print('Bot is ready')
+
+client.run(token.read()) # read the contents of the token file
