d8743ec1a3175c58904415c3d7ca5bbd16af97a2
bpetterborg <ben@ben-p.dev>
2021-04-16 15:11:08 -0400

    Added stuff to for running multiple pings

--- bot.py
+++ bot.py
@@ -8,21 +8,27 @@
 # NOTE:
 # You will need a file named 'token' with 
 # your token in the same directory of the bot
+#
+# TODO: Add audio functionality
+#
 
 # version number
 VERSION = '0.0.1'
-
 
 # imports
 import os           # not sure what this is
 import discord      # for the bot
 from discord.ext import commands	# to use commands to control bot
+from sultan.api import Sultan		# run commands to start p25 decoder
+
+from bot_commands import * # import all commands
+
 
 # variables
 client = commands.Bot(command_prefix = '.radio ') # the prefix to the command
 
-# get token (make sure token file is added to git ignore)
-token = open('token', 'r')
+# welcome message
+print(f'OHWG-Radio-Monitor V.{VERSION}')
 
 # COMMANDS:
 
@@ -31,51 +37,70 @@ token = open('token', 'r')
 async def on_ready(): # start when bot is in ready state
 	print('Ready \n')
 
+
 # get bot latency
-@client.command()
-async def ping(ctx):
-	# get latency
-	await ctx.send(f'\nPING OHWG-Radio-Monitor (#7044): \nTook {client.latency * 1000}ms to respond.')
-	print(f'Responding to ping. Took {client.latency * 1000}ms')
+@client.command(aliases=['--get-latency','--ping','-p'])
+async def ping(ctx, arg):
+	
+	# get latency TODO: Add -n flag for a loop
+	await ctx.send(f'\n PING OHWG-Radio-Monitor (#7044):')
+	TYPE_ERROR_DIALOG = 'TypeError: Please enter an integer.'
+
+	# if there is an arg, loop
+	if arg is int:
+		
+		for i in arg: # TODO: Fix this loop
+			print(f'Latency: {client.latency * 1000}ms')
+
+	elif arg is not int:
+		print(TYPE_ERROR_DIALOG)
+
+	elif arg == '': # NOTE: Test this
+		print(f'Latency: {client.latency * 1000}ms')
+
+	else:
+		print('Syntax Error: Check formatting')
 
 # print basic manpage
 @client.command(aliases=['--help','-h'])
 async def get_help(ctx):
+	
 	# list all commands (TODO: change this reading a file)
 	await ctx.send(open('help.md','r').read())
 	print('Printing help page')
 
+
 # kill bot
-@client.command()
+@client.command(aliases=['-H', '--halt'])
 async def halt(ctx):
+	
 	await ctx.send('\nExiting. Program restart required on server.')
 	print('Exiting')
 	exit()
 
-# set zone of radio
-#@client.command()
-#async def zone(ctx, arg):
-#	# add stuff to send command to set radio zone
-#	await ctx.send(f'Setting zone to {arg}')
-
 # set channel of radio
 @client.command()
 async def channel(ctx, arg):
+	
 	# TODO: Add stuff to send frequency change to op25
-	await ctx.send(f'Setting channel to {arg}')
+	await ctx.send(f'\nSetting channel to {arg}')
+
 
 @client.command(aliases=['-v','--version'])
 async def version(ctx):
-	await ctx.send(f'Radio Monitor Version {VERSION}')
+	await ctx.send(VERSION)
+
 
 # connect to VC
-@client.command(pass_context = True)
+@client.command(aliases=['-c', '--connect'], pass_context = True)
 async def connect(ctx):
 	
 	if (ctx.author.voice):
+		
 		# connect to channel
 		channel = ctx.message.author.voice.channel
 		await channel.connect()
+		
 		# logging
 		await ctx.send(f'Connecting to channel {channel}')
 		print(f'Connecting to VC.')
@@ -86,10 +111,11 @@ async def connect(ctx):
 
 
 # disconnecting from VC
-@client.command(pass_context = True)
+@client.command(aliases=['-D','--disconnect'], pass_context = True)
 async def disconnect(ctx):
 	
 	if (ctx.voice_client):
+		# if connected, don't be
 		await ctx.guild.voice_client.disconnect()
 		await ctx.send('Leaving VC')
 
@@ -98,5 +124,4 @@ async def disconnect(ctx):
 
 
 # run the bot
-client.run(token.read()) # read the contents of the token file
-
+client.run(open('token', 'r').read()) # read the contents of the token file
